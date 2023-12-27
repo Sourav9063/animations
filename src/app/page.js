bae70a092d1763e1de6b6ad0604ac2da2e352b97
Sourav9063 <sourav.ahmed5654@gmail.com>
2023-12-27 22:32:46 +0600

    home

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,36 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Stars from '@/components/stars/stars'
+import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
-   <Stars/>
-  )
+    <>
+      <nav>
+        <div>
+          <Link href={"/"}>Home</Link>
+        </div>
+        <div>
+          <h3>Simple</h3>
+          <ul>
+            <li>
+              <Link href={"/simple/transition"}>Transition</Link>
+            </li>
+            <li>
+              <Link href={"/simple/keyframe"}>Keyframe</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h3>Complex</h3>
+          <ul>
+            <li>
+              <Link href={"/complex"}>Stars</Link>
+            </li>
+            <li>
+              <Link href={"/complex"}>SVG Animation</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 }
